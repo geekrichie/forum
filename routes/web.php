79e775494a0787ae775f4e3e7f73efede4467aee
tilller <h4481695abcd@163.com>
2018-03-04 22:34:53 +0800

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::get('register','Auth\RegisterController@showRegistrationForm')->name('reg
 Route::post('register','Auth\RegisterController@register');
 
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
