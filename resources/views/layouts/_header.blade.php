d6feb882df89a0615f38fbc149331cbd514f74be
tilller <h4481695abcd@163.com>
2018-03-11 21:44:05 +0800

    List topics created by User

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -36,12 +36,22 @@
                     {{Auth::user()->name}}<span class="caret"></span>
                   </a>
                   <ul class="dropdown-menu" role="meun">
-                       <li>
-                          <a href="{{route('users.edit',Auth::user()->id)}}">修改信息</a>
-                       </li>
+                          <li>
+                                <a href="{{ route('users.show', Auth::id()) }}">
+                                   <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                    个人中心
+                                </a>
+                          </li>
+                           <li>
+                               <a href="{{ route('users.edit', Auth::id()) }}">
+                                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                                   编辑资料
+                               </a>
+                           </li>
                     <li>
                           <a href="{{route('logout')}}" onclick="event.preventDefault();
                                                                  document.getElementById('logout-form').submit()">
+                            <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
                           退出登录
                           </a>
                           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
