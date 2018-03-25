7dc7516108c8e9a9f35364d8e03e9cb784f55ace
tilller <h4481695abcd@163.com>
2018-03-25 15:31:02 +0800

    Add manage contents permission add visit horizon dashboard permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
           \App\Models\User::observe(\App\Observers\UserObserver::class);
-		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+		      \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
           \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
           \Carbon\Carbon::setLocale('zh');
     }
@@ -28,5 +28,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if(app()->isLocal()){
+             $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
     }
 }
