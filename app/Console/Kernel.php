0e1f77650d2b3885f3b59b96c521d754386b805e
tilller <h4481695abcd@163.com>
2018-03-30 19:54:42 +0800

    Keep track of user last visited time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,8 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         //一小时执行一次活跃用户数据生成的命令
         $schedule->command('larabbs:calculate-active-user')->hourly();
+        //每日0时执行一次
+        $schedule->command('larabbs:sync-user-actived-at')->dailyAt('00:00');
     }
 
     /**
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -15,7 +15,9 @@
                 <p>{{$user->introduction}}</p>
                 <hr>
                 <h4> <strong> 注册于</strong></h4>
-                <p>{{$user->created_at}}</p>
+                <p>{{ $user->created_at->diffForHumans() }}</p>
+                <h4> <strong> 最后活跃</strong></h4>
+                <p title="{{ $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
               </div>
             </div>
           </div>
